838a7b209d80ecd232de444343d2702570c76679
Raven :3 <anastasiapink2805@gmail.com>
2023-08-28 18:54:39 +0800

    Working Commit
    Switched from using strings for on to using Events
    Added @types/discord.d.ts because typescript loves to complain about Property 'commands' does not exist on type 'Client<boolean>'
    Added algorithm to register any command in ./src/commands
    Switched to using SlashCommandBuilder to register slash commands
    Added ping, reload, server, stop, user slash commands
    Known bugs: ping slash command is registered twice :c
    
    Commit took 3.5hrs

---
+++ src/commands/utils/server.ts
@@ -0,0 +1,44 @@
+import { EmbedBuilder, GuildMember, SlashCommandBuilder } from 'discord.js';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Provides information about the server.'),
+	async execute(interaction: {
+		guild: {
+			vanityURLCode: null;
+			fetchOwner(): Promise<GuildMember>;
+			name: string;
+			memberCount: string;
+		};
+		reply: (arg0: { embeds: EmbedBuilder[] }) => any;
+	}) {
+		try {
+			const vanityURL =
+				interaction.guild.vanityURLCode === null
+					? 'No Vanity URL'
+					: `https://discord.gg/${interaction.guild.vanityURLCode}`;
+
+			// interaction.guild is the object representing the Guild in which the command was run
+			const exampleEmbed = new EmbedBuilder()
+				.setTitle('Server Information')
+				// .setDescription('Some description here')
+				// .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+				.addFields(
+					{ name: ' ', value: `Server Name: ${interaction.guild.name}` },
+					{ name: ' ', value: `Members: ${interaction.guild.memberCount}` },
+					{ name: ' ', value: `Server Owner: ${await interaction.guild.fetchOwner()}` },
+					{ name: ' ', value: `Vanity URL: ${vanityURL}` }
+				)
+				// .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+				// .setImage('https://i.imgur.com/AfFp7pu.png')
+				.setTimestamp()
+				.setFooter({ text: 'Made by Raven :3' });
+
+			await interaction.reply({ embeds: [exampleEmbed] });
+		} catch (err) {
+			console.error(err);
+			await interaction.reply(err.message);
+		}
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,59 @@
-import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 import ready from './listener/ready';
+import path = require('path');
+import fs = require('fs');
 const TOKEN = process.env.BOT_TOKEN || '';
 const CLIENT_ID = process.env.CLIENT_ID || '';
-(async () => {
-	const commands = [{ name: 'ping', description: 'Replies with Pong!' }];
-	const rest = new REST({ version: '10' }).setToken(TOKEN);
+const COMMANDS_PATH = 'commands';
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Handle Slash Commands
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, COMMANDS_PATH);
+
+for (const folder of fs.readdirSync(foldersPath)) {
+	const commandsPath = path.join(foldersPath, folder);
+	// Loop
+	for (const file of fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'))) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.warn(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
+	}
+}
+
+// Slash command responses
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return; //Narrow type from BaseInteraction to ChatInputCommandInteraction
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) return console.error(`No command matching ${interaction.commandName} was found.`);
 
 	try {
-		console.info('Started refreshing application (/) commands.');
-
-		await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-		console.info('Successfully reloaded application (/) commands.');
+		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		}
 	}
-	const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-	ready(client);
-
-	client.on('interactionCreate', async interaction => {
-		if (!interaction.isChatInputCommand()) return;
-
-		if (interaction.commandName === 'ping') await interaction.reply('Pong!');
-	});
-
-	client.login(TOKEN);
-})();
+});
+// When the client is ready, run this code (only once)
+ready(client);
+// Log in to Discord with client's token
+client.login(TOKEN);
